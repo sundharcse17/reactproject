fcdd75227e843f943709c7516d92216de46bfcd6
sundharcse17 <sundharcse17@gmail.com>
2024-02-28 23:08:10 +0530

    added page.js in feature

---
+++ Page.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <div>
+        hai git
+       </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
